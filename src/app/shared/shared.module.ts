d6e012607523622e10f03b543d28e45f749bc931
Garrett Duncan <duncansports_17@hotmail.com>
2025-03-25 10:52:08 -0500

    Add ReactiveFormsModule to SharedModule for enhanced form handling

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { TaskStatusPipe } from './task-status.pipe';
 import { TaskHighlightDirective } from './task-highlight.directive';
 import { NotificationComponent } from '../notification/notification.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [TaskStatusPipe, TaskHighlightDirective, NotificationComponent],
   imports: [
-    CommonModule, FormsModule,
+    CommonModule, FormsModule, ReactiveFormsModule
   ],
   exports: [TaskStatusPipe, TaskHighlightDirective, NotificationComponent, CommonModule, FormsModule]
 })
